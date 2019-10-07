9afb35167b23a22bbf0f5847f0cd965109914a6a
Alex <thealex11011999@gmail.com>
2019-10-07 10:22:02 +0200

    Fragmentos01

---
+++ ProyectoF01/app/src/main/java/com/aalvarez/proyectof01/Fragment01.java
@@ -0,0 +1,14 @@
+package com.aalvarez.proyectof01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Fragment01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment01);
+    }
+}
---
+++ ProyectoF01/app/src/main/java/com/aalvarez/proyectof01/Fragmento1.java
@@ -0,0 +1,18 @@
+package com.aalvarez.proyectof01;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+
+public class Fragmento1 extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragmento1, container, false);
+    }
+}
