a1b2d494efcd82ac0eaf4e263746f8a8ba217164
thealexcod99 <thealex11011999@gmail.com>
2019-11-18 12:59:11 +0100

    ProyectoMAterial08

---
+++ ProyectoMaterial08/app/src/main/java/com/aalvarez/proyectomaterial08/Material08.java
@@ -0,0 +1,14 @@
+package com.aalvarez.proyectomaterial08;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Material08 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mateial08);
+    }
+}
