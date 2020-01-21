fba29f442a5f1ed0a863be2e4bc12d6fbd66c6f6
Alejandro Alvarez <alex@gmail.com>
2020-01-21 10:23:13 +0100

    Proyectos de persistencia

--- ProyectoPersistencia05/app/src/main/java/com/example/proyectopersistencia05/MainActivity.java
+++ ProyectoPersistencia05/app/src/main/java/com/example/proyectopersistencia05/MainActivity.java
@@ -2,21 +2,25 @@ package com.example.proyectopersistencia05;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.stream.Collectors;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
     private TextView textView;
+    private String texto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,27 +33,60 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                /*final Thread thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            URL url = new URL("https://www.google.com/humans.txt");
+                            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                            Integer estado = urlConnection.getResponseCode();
+                            Toast.makeText(getApplicationContext(), estado.toString(), Toast.LENGTH_LONG).show();
+                            *//*InputStreamReader inputStreamReader = new InputStreamReader(urlConnection.getInputStream());
+                            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                                texto = bufferedReader.lines().collect(Collectors.joining());
+
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        textView.setText(texto);
+                                    }
+                                });
+                            }*//*
+                            urlConnection.disconnect();
+
+                        } catch (Exception ex) {
+                            Log.e("FICHEROS", "Error al leer datos en la red");
+                        }
+                    }
+                });*/
+
                 try {
                     URL url = new URL("https://www.google.com/humans.txt");
                     HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-                    urlConnection.setReadTimeout(10000);
-                    urlConnection.setConnectTimeout(15000);
-                    urlConnection.setRequestMethod("GET");
-                    urlConnection.setDoInput(true);
                     urlConnection.connect();
-                    StringBuffer strBuff = new StringBuffer();
-                    BufferedReader fichRedH = new BufferedReader(
-                        new InputStreamReader(urlConnection.getInputStream(), "UTF-8"));
-                    String texto;
-                    while ((texto = fichRedH.readLine()) != null) {
-                        strBuff.append(texto);
-                    }
-                    fichRedH.close();
-                    textView.setText(strBuff.toString());
+                    Integer estado = urlConnection.getResponseCode();
+                    Toast.makeText(getApplicationContext(), estado.toString(), Toast.LENGTH_LONG).show();
+                            /*InputStreamReader inputStreamReader = new InputStreamReader(urlConnection.getInputStream());
+                            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                                texto = bufferedReader.lines().collect(Collectors.joining());
+
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        textView.setText(texto);
+                                    }
+                                });
+                            }*/
+                    urlConnection.disconnect();
 
                 } catch (Exception ex) {
                     Log.e("FICHEROS", "Error al leer datos en la red");
                 }
+
             }
         });
 
